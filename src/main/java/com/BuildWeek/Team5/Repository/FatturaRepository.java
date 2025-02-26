9293dc4f60cf47446b62b09e91eaa98a106734f9
Doc-Franz <federico.franzi94@gmail.com>
2025-02-26 13:05:48 +0100

    fatture per cliente

--- src/main/java/com/BuildWeek/Team5/Repository/FatturaRepository.java
+++ src/main/java/com/BuildWeek/Team5/Repository/FatturaRepository.java
@@ -2,9 +2,12 @@ package com.BuildWeek.Team5.Repository;
 
 import com.BuildWeek.Team5.Model.Fattura;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FatturaRepository extends JpaRepository<Fattura, Long> {
-    List<Fattura> findByIdCliente(Long idCliente);
+    @Query("SELECT f FROM Fattura f WHERE f.id IN (SELECT fa.id FROM Cliente c JOIN c.fatture fa WHERE c.idCliente = :idCliente)")
+    List<Fattura> findByIdCliente(@Param("idCliente") Long idCliente);
 }
